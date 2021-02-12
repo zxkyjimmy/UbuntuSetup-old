dd0e0ff03cf756ab79fa83412ec2aff697bf6f0a
Yen-Chi Chen <zxkyjimmy@gmail.com>
2021-02-12 17:17:54 +0800

    bazel

--- setup.sh
+++ setup.sh
@@ -62,3 +62,11 @@ sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/
 sudo apt update
 sudo apt install -y cuda-11-2
 sudo apt install -y libcudnn8 libcudnn8-dev
+
+step "Install Bazel"
+sudo apt install -y curl gnupg
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
+sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update
+sudo apt install -y bazel
