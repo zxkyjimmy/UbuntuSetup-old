79e4096bc81eace17691fbf4ce9616f6dbcec409
Yen-Chi Chen <zxkyjimmy@gmail.com>
2021-02-13 12:31:31 +0800

    nvidia-container-runtime

--- setup.sh
+++ setup.sh
@@ -80,6 +80,14 @@ sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install podman
 
+step "Install nvidia-container-runtime"
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+
 step "clean up"
 sudo apt update
 sudo apt upgrade -y
