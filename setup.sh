4550047442238f4cc44b3d435915c4c8a0aeca0d
Yen-Chi Chen <zxkyjimmy@gmail.com>
2021-09-07 16:39:56 +0800

    cuda auto confirm

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ step "Get CUDA"
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo add-apt-repository -y "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt update
 sudo apt install -y cuda-drivers
 sudo apt install -y cuda-11-4
