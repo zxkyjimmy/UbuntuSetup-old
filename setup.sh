a6fa5d1dcd43ad759e7677c372f87e3be98c4389
Yen-Chi Chen <zxkyjimmy@gmail.com>
2022-07-03 16:36:14 +0800

    yes for bazel

--- setup.sh
+++ setup.sh
@@ -99,7 +99,7 @@ sudo apt install -y cuda-11-7
 sudo sed -E 's;PATH="?(.+)";PATH="/usr/local/cuda/bin:\1";g' -i /etc/environment
 
 step "Install Bazel"
-sudo apt install apt-transport-https curl gnupg
+sudo apt install -y apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel-archive-keyring.gpg
 sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
